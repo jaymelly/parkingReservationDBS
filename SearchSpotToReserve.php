d2a489e47ae72de6fa3f21dbe44c22ca531a8901
dperry8 <123112296+dperry8@users.noreply.github.com>
2023-11-27 13:01:20 -0500

    Fixed initial bug

--- SearchSpotToReserve.php
+++ SearchSpotToReserve.php
@@ -4,18 +4,20 @@
 <h1>Search for Open Zones</h1>
 <body>
     <form name="form" action="" method="post">
-        <input type="text" name="user_input" size="35" placeholder="Enter distance or date in YYYYDDMM format">
+        <input type="text" name="user_input" size="35" placeholder="Enter distance or date in YYYYDDMM format" required>
         <input type="submit">
     </form>
 <?php
     // Connection
     $sql = "";
+    $input = "";
     $conn = new mysqli('localhost', 'phpuser', 'phpwd', 'PARKING');
     // Get date and check valid input
-    $input = $_POST['user_input'];
-    if (empty($input)) {
-        echo "Not a valid input";
-    } else {
+    if (isset($_POST['user_input'])) {
+        $input = $_POST['user_input'];
+    }
+    if (!empty($input)) {
+        // Change from spots taken to checking avaiable spots (des-taken)
         $sql = sprintf("select ZONE_NUM, DESIGNATED_SPOTS, SPOTS_TAKEN, RATE from ZONE where (ZONE_DATE=%s or DISTANCE=%s) and SPOTS_TAKEN>0", $input, $input);
         $result = mysqli_query($conn, $sql);
         echo "<table style=\"width:30%\">";
@@ -30,12 +32,14 @@
             echo "<td style=\"border:1px solid\">". $row['SPOTS_TAKEN']. "</td>";
             echo "<td style=\"border:1px solid\">". $row['RATE']. "</td>";
             if (date("Y-m-d", strtotime($input)) != date("Y-m-d")) {
-            echo "<td style=\"border:none\"><a href=\"ReserveSpot.php\"><button>Reserve</button></a></td>";
+                // Change file name to Jack's Make reservation file name
+            echo "<td style=\"border:none\"><a href=\"makeReservation.php\"><button>Reserve</button></a></td>";
             }
             echo "</tr>";
         }
         echo "</table>";
     }
 ?>
+
 </body>
 </html>
