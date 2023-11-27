377b83ee44926c7d1532e3175ee92a0837d92d0a
dperry8 <123112296+dperry8@users.noreply.github.com>
2023-11-27 15:33:19 -0500

    Done hopefully

--- SearchSpotToReserve.php
+++ SearchSpotToReserve.php
@@ -20,29 +20,42 @@ if (isset($_POST['user_input'])) {
 
 // Do sql if there is an input
 if (!empty($input)) {
-    // Change from spots taken to checking avaiable spots (des-taken)>0
-    $sql = sprintf("select ZONE_NUM, DESIGNATED_SPOTS, SPOTS_TAKEN, RATE from ZONE where (ZONE_DATE=%s or DISTANCE=%s) and SPOTS_TAKEN>0", $input, $input);
+    // Deal with distance
+    $sql = sprintf("select ZONE_NUM, DESIGNATED_SPOTS, SPOTS_TAKEN, RATE from ZONE where (ZONE_DATE=%d or DISTANCE=%d) and (DESIGNATED_SPOTS-SPOTS_TAKEN)>0", $input, $input);
     $result = mysqli_query($conn, $sql);
+    // Checks if there are any results
+    if (mysqli_num_rows($result)==0) {
+        echo "No Results Found. Please Check Formatting and Try Again.";
+    // Continue with code
+    } else {
 ?>
+    <!-- Print table header -->
     <table style="width:30%">
-    <tr>
-        <th style="border:1px solid">Zone Number</th>
-        <th style="border:1px solid">Spots Available</th>
-        <th style="border:1px solid">Rates</th>
-    </tr>
-<?php
+        <tr>
+            <th style="border:1px solid">Zone Number</th>
+            <th style="border:1px solid">Spots Available</th>
+            <th style="border:1px solid">Rates</th>
+        </tr>
+    <?php
+        // Loop and print results of query into table
         while ($row = mysqli_fetch_array($result)) {
-
+            
+            // Get data to send to reservation
             $zone = $row['ZONE_NUM'];
+            $rate = $row['RATE'];
+            $date = $input;
 
+            // Prints table information
             echo "<tr>";
             echo "<td style=\"border:1px solid\">". $row['ZONE_NUM']. "</td>";
-            // Need to print (des-taken)
-            echo "<td style=\"border:1px solid\">". $row['SPOTS_TAKEN']. "</td>";
+            echo "<td style=\"border:1px solid\">". ($row['DESIGNATED_SPOTS']-$row['SPOTS_TAKEN']). "</td>";
             echo "<td style=\"border:1px solid\">". $row['RATE']. "</td>";
+            //  Checks if user is attempting to reserve date on the same day. If so, it does not display reserve button
             if (date("Y-m-d", strtotime($input)) < date("Y-m-d")) {
-                // Change file name to Jack's Make reservation file name
+                // Change file name to Jack's Make reservation file name and delete this comment
                 echo "<form method=post action=\"ReserveASpot.php\">";
+                echo "<input type='hidden' name='date' value=$date>";
+                echo "<input type='hidden' name='rate' value=$rate>";
                 echo "<td style=\"border:none\"><button name='zone_num' value=$zone>Reserve</button></a></td>";
                 echo "</form>";
             }
@@ -50,6 +63,7 @@ if (!empty($input)) {
         }
         echo "</table>";
     }
+}
 ?>
 </body>
 </html>
