fd8b89f30c1c5a00bba6b3585b1b3e888a529e5e
EmmanuelKey <88858595+EmmanuelKey@users.noreply.github.com>
2023-11-27 12:04:15 -0500

    Add files via upload

---
+++ front.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Question</title>
+</head>
+<body>
+    <h1>Are you an admin?</h1>
+    <p>
+        <a href="index.php">Yes</a> | 
+        <a href="user.html">No</a>
+    </p>
+</body>
+</html>
