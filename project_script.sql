3612a14b0db04c76a5d8dfbcb0e6cbf966056708
dperry8 <123112296+dperry8@users.noreply.github.com>
2023-11-28 16:13:45 -0500

    Update project_script.sql

--- project_script.sql
+++ project_script.sql
@@ -32,6 +32,6 @@ CREATE TABLE IF NOT EXISTS DISTANCE(
 );
 
 
-insert into DISTANCE values (1, "Nationwide Arena", 1), (1, "COSI", 4), (1, "Huntington Park", 3), (2, "Nationwide Arena", 2), (2, "COSI", 5), (2, "Huntington Park", 4), (3, "Nationwide Arena", 2), (3, "COSI", 4), (2, "Huntington Park", 2); 
+insert into DISTANCE values (1, "Nationwide Arena", 1), (1, "COSI", 4), (1, "Huntington Park", 3), (2, "Nationwide Arena", 2), (2, "COSI", 5), (1, "Huntington Park", 4), (3, "Nationwide Arena", 2), (3, "COSI", 4), (2, "Huntington Park", 2); 
 load data infile 'reservation' into table reservations;
 load data infile 'zone' into table zones;
