57890b01739b70455b93c61d26ce201b97305180
dperry8 <123112296+dperry8@users.noreply.github.com>
2023-11-28 12:56:02 -0500

    Combined front and login

--- loginPage.php
+++ loginPage.php
@@ -7,6 +7,8 @@
 </head>
 <body>
     <h1>Login</h1>
+    <!---------------------------------------------------- Admin Login -------------------------------------------------------------------->
+    <h2>Enter as Admin</h2>
 
     <form method="post" action="">
         <label for="username">Username:</label>
@@ -27,10 +29,14 @@ if (!empty($username) && !empty($password)) {
     if ($username=="admin" && $password=="admin123") {
         header("Location: admin.php");
     } else {
-        echo "<br><h2>Invalid username or password. Please try again.<h2>";
+        echo "<br>Invalid username or password. Please try again.<br>";
     }
 }
 ?>
 
+<!---------------------------------------------------- User Login -------------------------------------------------------------------->
+<br><br><br><br>
+<h2>Enter as User</h2>
+<a href="UserInterface.php"><button>Go To User Page</button> 
 </body>
 </html>
