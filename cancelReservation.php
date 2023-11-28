fe31e73e07b03701705af7790a19abb975946c11
jaymelly <123986761+jaymelly@users.noreply.github.com>
2023-11-28 15:41:34 -0500

    Update cancelReservation.php

--- cancelReservation.php
+++ cancelReservation.php
@@ -1,9 +1,9 @@
 <?php
 include 'reservation.php';
-session_start();
-$phone;
-$newRes;
+$confirm_num;
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -12,95 +12,73 @@ $newRes;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="user.css">
-    <title>Make Reservation</title>
+    <title>Cancel Reservation</title>
+
+    
 </head>
 <body>
     <?php
         //$conn = new mysqli("127.0.0.1", "root", "mySQLmySQL", "ParkingReservations");
         $conn = new mysqli('127.0.0.1', 'phpuser', 'phpwd', 'cse3241_project');
+        $cancel_status = false;
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!empty($_POST['date']) && !empty($_POST['rate']) && !empty($_POST['zone_num'])) {
-                $_SESSION['date'] = $_POST['date'];
-                $_SESSION['zone'] = $_POST['zone_num'];
-                $_SESSION['fee']= $_POST['rate'];  
-                
-
+            if (!empty($_POST['$confirm_num'])) {
+                $confirm_num = $_POST['confirmation_num'];
             }
-            if(!empty($_POST['phone'])){
-                $phone = $_POST['phone'];
-                $newRes = new Reservation($_SESSION['zone'], $phone, $_SESSION['date'], $_SESSION['fee'], $conn);
-                $newRes -> generateUniqueCnf();
-            }
-            $zone = $_SESSION['zone'];
-            $fee = $_SESSION['fee'];
-            $date = $_SESSION['date'];
         }
     ?>
-
+    
     <div class="header-container">
-        <header class="header">Confirm Reservation</header>
+        <header class="header">Cancel Reservation</header>
     </div>
 
     <?php
-        if(isset($zone) && isset($fee)){
-            echo "<p class = \"sentence\"> Your selected Zone: $zone </p>";
-            echo "<p class = \"sentence\"> Your Selected Zone Rate: $fee </p>";
+        if(isset($confirm_num)){
+            echo "<p class = \"sentence\"> You have chosen to cancel your reservation: $confirm_num </p>";
+            $tempRes = new Reservation(null, null, null, null, $conn);
+            $tempRes->setResConfirm($confirm_num);
+
+            echo "<p class = \"sentence\"> Reservation Confirmation #:  $confirm_num </p>";
+            echo "<p class = \"sentence\"> Reservation Zone: $tempRes->zone_num </p>";
+            echo "<p class = \"sentence\"> Reservation Date:  $tempRes->date_reserved </p>";
+            echo "<p class = \"sentence\"> Reservation Fee:  $tempRes->fee </p>";
+            echo "<p class = \"sentence\"> Are you sure you wish to cancel? </p>";
+
+            $tempRes->cancelReservation();
+            
+        } else {
+            echo "<p class = \"sentence\">  No reservation selected to cancel. </p>";
         }
-       
-    ?>
-
-    <form method="post">
-        <label for="input_usr_reservation">Please Enter a Phone Number for this reservation: </label>
-        <input type="text" name="phone" id="input_usr_reservation" required>
-
         
-
-        <!--
-        <label for="input_usr_reservation">Please Enter a name: </label> 
-        <input type="text" name="input_usr_phone" id="input_usr_date" required>
-        -->
-        <?php
-        if(!isset($phone)){
-            echo "<p style=\"color: red;\" > Please Enter Phone Number! </p>";
-        } else if (!isset($_SESSION['date'])){
-            echo "<p style=\"color: red;\" > Date not imported! </p>";
-        } else if (!isset($_SESSION['fee'])){
-            echo "<p style=\"color: red;\" > Fee did not import </p>";
-        } else if(!isset($_SESSION['zone'])){
-            echo "<p style=\"color: red;\" > Zone did not import </p>";
-        }
-        else {
-            $status = false;
-            if(isset($newRes)){
-
-                $status = $newRes->insertReservation();
-                if($status) echo "<p> Reservation Made successfully! </p>";
-                else echo "<p style=\"color: red;\" > Reservation failed to create! </p>";
-            }
-        }
-       
-        $conn -> close();
-        ?>
-
-        <div>
-            <button class = "button" type="submit" name="submitButton">Confirm Reservation</button>
+    ?>
+    <div class="button-container"> 
+        <form method="post" action="userInterface.php">
+            <button class = "button" type="submit" name="cancelButton" onclick="showPopup()">Confirm Cancellation</button>
             <button class = "button" type="" name="goBackButton" onclick="goBack()">Go Back</button>
-            <button class = "button" type="" name="nextButton" onclick="goToNewReservations()">New Reservation</button>
-        </div>
-    </form>
+            <button class = "button" type="" name="nextButton" onclick="goToNewReservations()">New Reservations</button>
+        </form>
+        
+    </div>
+    <?php $currentDateTime = date("Y-m-d H:i:s");?>
+    <p>Current Date and Time: <?php echo $currentDateTime; ?></p>
+
     
-    
+    <?php
+        $conn -> close();
+    ?>
     <script>
         function showPopup() {
-            alert("Your reservation has been Confirmed! \nPress go back to see your other reservations, or make another reservation to reserve a new spot."); 
+            alert("Your reservation has been cancelled! \nPress go back to see your other reservations, or make a new reservation to reserve a new spot."); 
+            window.location.href = "userInterface.php";
         }
         function goBack() {
-            window.location.href = "UserInterface.php"; 
+            window.location.href = "userInterface.php"; 
         }
         function goToNewReservations() {
-            window.location.href = "UserInterface.php"; 
+            window.location.href = "userInterface.php"; 
         }
     </script>
+
 
 </body> 
 </html>
