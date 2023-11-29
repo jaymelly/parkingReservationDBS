3e025e78d6dd3d9153ae093c354d16b6b2da3a6d
skyFzz <103511866+skyFzz@users.noreply.github.com>
2023-11-29 10:43:07 -0500

    Added Userinterface.php

--- UserInterface.php
+++ UserInterface.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="UserInterface.css">
 <title>User Page</title>
 <!-- Styling of columns -->
 <style>
@@ -19,31 +21,28 @@
   display: table;
   clear: both;
 }
-
-.vl {
-  border-left: 2px solid black;
-  height: 500px;
-  position: absolute;
-  left: 50%;
-  margin-left: -3px;
-  top: 0;
-}
 </style>
 </head>
 <body>
-
+<header class="header" style="font-size: 40px; margin-left: 10px">User Page</header>
+<section class="sidebar">
+                <div class="grid-item"><a href="index.php">Home</a></div>
+                <div class="grid-item"><a href="UserInterface.php">User Page</a></div>
+                <div class="grid-item"><a href="login.php">Admin Portal</a></div>
+</section>
+<main class="main">
 <!---------------------------------------------------- Search for spot to reserve -------------------------------------------------------------------->
 <div class="column">
 <h1>Search for Open Zones</h1>
 <form name="form" action="" method="post">
-    <input type="text" name="user_input_search" onsubmit=true size="55" value="<?= (isset($_POST['user_input_search']) ? $_POST['user_input_search'] : '');?>" placeholder="Enter admired distance from zone or date in YYYYDDMM format" required>
+    <input type="text" name="user_input_search" size="55" placeholder="Enter admired distance from zone or date in YYYYDDMM format" required>
     <input type="submit">
 </form>
 <?php
 // Connection
 $sql = "";
 $input = "";
-$conn = new mysqli('localhost', 'phpuser', 'phpwd', 'PARKING');
+$conn = new mysqli('127.0.0.1', 'phpuser', 'phpwd', 'cse3241_project');
 
 // Check if input is set yet
 if (isset($_POST['user_input_search'])) {
@@ -53,7 +52,7 @@ if (isset($_POST['user_input_search'])) {
 // Do sql if there is an input
 if (!empty($input)) {
     // Gets SQL query results
-    $sql = sprintf("select z.ZONE_NUM, DESIGNATED_SPOTS, SPOTS_TAKEN, RATE, d.DISTANCE from ZONE z, DISTANCE d where z.ZONE_NUM = d.ZONE_NUM and (ZONE_DATE=%d or d.DISTANCE=%d) and (DESIGNATED_SPOTS-SPOTS_TAKEN)>0", $input, $input);
+    $sql = sprintf("select z.ZONE_NUM, DESIGNATED_SPOTS, SPOTS_TAKEN, RATE, d.DISTANCE, z.Zone_date, d.EVENT from ZONES z, DISTANCE d where z.ZONE_NUM = d.ZONE_NUM and (ZONE_DATE=%d or d.DISTANCE=%d) and (DESIGNATED_SPOTS-SPOTS_TAKEN)>0", $input, $input);
     $result = mysqli_query($conn, $sql);
     // Checks if there are any results
     if (mysqli_num_rows($result)==0) {
@@ -62,11 +61,13 @@ if (!empty($input)) {
     } else {
 ?>
     <!-- Print table header -->
-    <table style="width:75%">
+    <table style="width:100%">
         <tr>
             <th style="border:1px solid">Zone Number</th>
             <th style="border:1px solid">Spots Available</th>
             <th style="border:1px solid">Rates</th>
+            <th style="border:1px solid">Date</th>
+            <th style="border:1px solid">Event</th>
         </tr>
     <?php
         // Loop and print results of query into table
@@ -75,22 +76,24 @@ if (!empty($input)) {
             // Get data to send to reservation
             $zone = $row['ZONE_NUM'];
             $rate = $row['RATE'];
-            $date = $input;
+            $date = $row['Zone_date'];
+	    $event = $row['EVENT'];
 
             // Prints table information
             echo "<tr>";
             echo "<td style=\"border:1px solid\">". $row['ZONE_NUM']. "</td>";
             echo "<td style=\"border:1px solid\">". ($row['DESIGNATED_SPOTS']-$row['SPOTS_TAKEN']). "</td>";
             echo "<td style=\"border:1px solid\">". $row['RATE']. "</td>";
+            echo "<td style=\"border:1px solid\">". $row['Zone_date']. "</td>";
+            echo "<td style=\"border:1px solid\">". $row['EVENT']. "</td>";
             //  Checks if user is attempting to reserve date on the same day. If so, it does not display reserve button
             if (date("Y-m-d", strtotime($input)) < date("Y-m-d")) {
+                // Change file name to Jack's Make reservation file name and delete this comment
                 echo "<form method=post action=\"confirmReservation.php\">";
                 echo "<input type='hidden' name='date' value=$date>";
                 echo "<input type='hidden' name='rate' value=$rate>";
                 echo "<td style=\"border:none\"><button name='zone_num' value=$zone>Reserve</button></td>";
                 echo "</form>";
-            } else {
-                echo "<td style=\"border:none\"><button disabled>Reserve</button></td>";
             }
             echo "</tr>";
         }
@@ -99,19 +102,18 @@ if (!empty($input)) {
 }
 ?>
 </div>
-<div class="v1"></div>
 <!---------------------------------------------------- Search for your reservations -------------------------------------------------------------------->
 <div class="column">
 <h1>Search for Your Reservation</h1>
 <form name="form" action="" method="post">
-    <input type="text" name="user_input_reservation" onsubmit="return false" size="38" value="<?= (isset($_POST['user_input_reservation']) ? $_POST['user_input_reservation'] : '');?>" placeholder="Enter Phone Number or Confirmation Number">
+    <input type="text" name="user_input_reservation" size="38" placeholder="Enter Phone Number or Confirmation Number">
     <input type="submit">
 </form>
 <?php
 // Connection
 $sql = "";
 $input = "";
-$conn = new mysqli('localhost', 'phpuser', 'phpwd', 'PARKING');
+$conn = new mysqli('127.0.0.1', 'phpuser', 'phpwd', 'cse3241_project');
 
 // Check if input is set yet
 if (isset($_POST['user_input_reservation'])) {
@@ -120,7 +122,8 @@ if (isset($_POST['user_input_reservation'])) {
 
 if (!empty($input)) {
     // Gets SQL query results
-    $sql = sprintf("select * from RESERVATION where PHONE=%d or CONFIRMATION_NUM=%d", $input, $input);
+    intval($input);
+    $sql = sprintf("select * from RESERVATIONS where PHONE=%s or Confirmation_id=%s", $input, $input);
     $result = mysqli_query($conn, $sql);
     // Checks if any results. If not, try again
     if (mysqli_num_rows($result)==0) {
@@ -129,35 +132,35 @@ if (!empty($input)) {
     } else {
 ?>
     <!-- Print table header -->
-    <table style="width:90%">
+    <table style="width:75%">
     <tr>
-        <th style="border:1px solid">Phone Number</th>
-        <th style="border:1px solid">Date Reserved</th>
+        <th style="border:1px solid">Confirmation_id</th>
+        <th style="border:1px solid">Zone_number</th>
+        <th style="border:1px solid">Phone</th>
+        <th style="border:1px solid">Cancelled</th>
+        <th style="border:1px solid">Date_reserved</th>
         <th style="border:1px solid">Fee</th>
-        <th style="border:1px solid">Zone Number</th>
-        <th style="border:1px solid">Confirmation Number</th>
-        <th style="border:1px solid">Cancellation Status</th>
     </tr>
 <?php
         // Prints results of query into table
         while ($row = mysqli_fetch_array($result)) {
 
             // Get data to send to cancellation
-            $confirmation_num = $row['CONFIRMATION_NUM'];
+            $confirmation_num = $row[0];
 
             // Prints table information
             echo "<tr>";
-            echo "<td style=\"border:1px solid\">". $row['PHONE']. "</td>";
-            echo "<td style=\"border:1px solid\">". $row['DATE_RESERVED']. "</td>";
-            echo "<td style=\"border:1px solid\">". $row['FEE']. "</td>";
-            echo "<td style=\"border:1px solid\">". $row['ZONE_NUM']. "</td>";
-            echo "<td style=\"border:1px solid\">". $row['CONFIRMATION_NUM']. "</td>";
-            // Checks if button is already canceled, if it is then option is disabled and prints cancelled in column
-            if ($row['CANCELLED'] == true) {
+            echo "<td style=\"border:1px solid\">". $row[0]. "</td>";
+            echo "<td style=\"border:1px solid\">". $row[1]. "</td>";
+            echo "<td style=\"border:1px solid\">". $row[2]. "</td>";
+            echo "<td style=\"border:1px solid\">". $row[3]. "</td>";
+            echo "<td style=\"border:1px solid\">". $row[4]. "</td>";
+            // Checks if button is already canceled, if it is then option is disabled
+		if ($row[3] == true) {
                 echo "<td style=\"border:1px solid\">Cancelled</td>";
                 echo "<td style=\"border:none\"><button disabled>Cancel</button></td>";
             } else {
-                if (date("Y-m-d", strtotime($row['DATE_RESERVED'])) >= date('Y-m-d', strtotime(' - 2 days'))) {
+                if (date("Y-m-d", strtotime($row[4])) >= date('Y-m-d', strtotime(' - 2 days'))) {
                     echo "<td style=\"border:1px solid\"></td>";
                     echo "<td style=\"border:none\"><button disabled>Cancel</button></td>";
                 } else {
@@ -174,5 +177,7 @@ if (!empty($input)) {
 }
 ?>
 </div>
+</main>
+
 </body>
 </html>
